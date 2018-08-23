77777ec7d7523177c676e62c86658e334f9960df
Chris Franklin <cf73021@gmail.com>
2018-08-23 17:41:38 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -8,7 +8,7 @@ class Patient
   end
 
   def new_appointment(doctor, date)
-    new_appointment = Appointment.new(self, self, date)
+    new_appointment = Appointment.new(self, doctor, date)
   end
 
 end
